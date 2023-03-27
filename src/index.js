a8a65eb4ebd9db096f802250a236190a0aa1b080
ihara <ihar.anikeyenka@gmail.com>
2023-03-28 01:29:23 +0200

    Create a politics page

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import { store } from './store';
+import {store} from './store';
 
 import reportWebVitals from './reportWebVitals';
+import {PoliticsPage} from "./pages/main/PoliticsPage";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <App/>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
