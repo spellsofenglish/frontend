9ecddd8ada7b3f55205176f29d2c967ff20ecfab
ihara <ihar.anikeyenka@gmail.com>
2023-04-04 16:47:52 +0200

    Create a modal window

--- src/pages/main/forms/sendEmail/sendEmail.js
+++ src/pages/main/forms/sendEmail/sendEmail.js
@@ -2,10 +2,13 @@
  * @prettier
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 import { QuestionBlock, TitleForm, InputForm, InputBtn } from '../../../../ui-kit';
+import { Modal } from '../../../../features/Modal';
+
+import letterImg from '../../../../assets/images/letter.png';
 
 const SendEmail = (props) => {
   const {
@@ -24,59 +27,67 @@ const SendEmail = (props) => {
     reset();
   };
 
-  const openModal = () => {
-    console.log('Открытие модального окна с текстом: Ссылка отправлена');
-  };
+  const [modalActive, setModalActive] = useState(false);
 
   return (
-    <div className="form">
-      <div className="form__logo" />
-      <div className="form__wrapper">
-        <div className="form__block">
-          <TitleForm text="Восстановление пароля" />
-          <form onSubmit={handleSubmit(onClickSendEmail)}>
-            <p className="form__recovery">
-              Введи email, чтобы мы могли отправить ссылку на восстановление пароля
-            </p>
-            <InputForm
-              label="Email"
-              name="email"
-              type="email"
-              placeholder="Ivan@example.com"
-              errors={errors}
-              register={register}
-              validationSchema={{
-                required: 'Поле обязательно для заполнения',
-                maxLength: {
-                  value: 50,
-                  message: 'Email должен содержать не более 50 латинских букв, цифр или символов',
-                },
-                pattern: {
-                  value: /(^|\s+)[\w\-.]+@([\w-]+\.)+[\w-]{2,4}($|\s+)/,
-                  message: 'Формат email неверный',
-                },
-                validate: {
-                  noSpace: (value) =>
-                    value.trim().includes(' ') === false ||
-                    'Your password must not contain spaces!',
-                },
-              }}
-            />
-            <InputBtn
-              disabled={isValid ? false : true}
-              type="submit"
-              value="Отправить ссылку"
-              onClick={openModal}
-            />
-          </form>
+    <>
+      <div className="form">
+        <div className="form__logo" />
+        <div className="form__wrapper">
+          <div className="form__block">
+            <TitleForm text="Восстановление пароля" />
+            <form onSubmit={handleSubmit(onClickSendEmail)}>
+              <p className="form__recovery">
+                Введи email, чтобы мы могли отправить ссылку на восстановление пароля
+              </p>
+              <InputForm
+                label="Email"
+                name="email"
+                type="email"
+                placeholder="Ivan@example.com"
+                errors={errors}
+                register={register}
+                validationSchema={{
+                  required: 'Поле обязательно для заполнения',
+                  maxLength: {
+                    value: 50,
+                    message: 'Email должен содержать не более 50 латинских букв, цифр или символов',
+                  },
+                  pattern: {
+                    value: /(^|\s+)[\w\-.]+@([\w-]+\.)+[\w-]{2,4}($|\s+)/,
+                    message: 'Формат email неверный',
+                  },
+                  validate: {
+                    noSpace: (value) =>
+                      value.trim().includes(' ') === false ||
+                      'Your password must not contain spaces!',
+                  },
+                }}
+              />
+              <InputBtn
+                disabled={!isValid}
+                type="submit"
+                value="Отправить ссылку"
+                onClick={() => {
+                  console.log(InputForm.value);
+                  setModalActive(true);
+                }}
+              />
+            </form>
+          </div>
+          <QuestionBlock
+            text="У меня уже есть аккаунт. "
+            textBtn="Войти в аккаунт"
+            onClick={() => props.setFormToStart(1)}
+          />
         </div>
-        <QuestionBlock
-          text="У меня уже есть аккаунт. "
-          textBtn="Войти в аккаунт"
-          onClick={() => props.setFormToStart(1)}
-        />
       </div>
-    </div>
+      <Modal active={modalActive} setActive={setModalActive} src={letterImg}>
+        <h2>Ссылка отправлена</h2>
+        <p>Мы отправили ссылку на восстановление пароля на example@gmail.com.</p>
+        <p>Cледуй инструкциям письма и ты быстро вернешься в игру!</p>
+      </Modal>
+    </>
   );
 };
 
