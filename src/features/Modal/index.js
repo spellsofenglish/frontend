a473a478122eb0df1b37a6d905eb7ee39bf4ff49
ihara <ihar.anikeyenka@gmail.com>
2023-04-05 18:54:02 +0200

    Added background for modal window

--- src/features/Modal/index.js
+++ src/features/Modal/index.js
@@ -9,15 +9,17 @@ import { Button } from '../../ui-kit';
 
 export const Modal = ({ active, setActive, children, src, alt }) => {
   return (
-    <div className={active ? 'modal active' : 'modal'}>
-      <div className="modal__close" onClick={() => setActive(false)}>
-        <div className="close__btn" />
-      </div>
-      <img className="modal__img" src={src} alt={alt} />
-      <div className="modal__content" onClick={(e) => e.stopPropagation()}>
-        {children}
-        <div className="modal__btn">
-          <Button label="Закрыть" size="medium" onClick={() => setActive(false)} />
+    <div className={active ? 'modal__background' : null}>
+      <div className={active ? 'modal active' : 'modal'}>
+        <div className="modal__close" onClick={() => setActive(false)}>
+          <div className="close__btn" />
+        </div>
+        <img className="modal__img" src={src} alt={alt} />
+        <div className="modal__content" onClick={(e) => e.stopPropagation()}>
+          {children}
+          <div className="modal__btn">
+            <Button label="Закрыть" size="medium" onClick={() => setActive(false)} />
+          </div>
         </div>
       </div>
     </div>
