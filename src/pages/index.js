b7335a43944a9afc60e2af75f7af9f3f435f503f
zatrimaylova <m.zatrimaylova@gmail.com>
2023-07-21 20:40:38 +0300

    SE-89 resolved conflict in pages index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,6 @@ import Menu from './menu';
 import Authorization from './authorization';
 import Storybook from './storybook';
 import Politics from './politics';
-<<<<<<< HEAD
-
-export { Menu, Authorization, Storybook, Politics };
-=======
 import NewGame from './new-game';
 
 export { Menu, Authorization, Storybook, Politics, NewGame };
->>>>>>> c214502670e7258bffa3d8272fd9a131f5ddae6a
